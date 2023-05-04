aa88ca421eeee2d9191c62492a905bbeeb583980
Vladimir Dzekh <dzeh1994@gmail.com>
2023-05-04 09:23:46 +0300

    Creating a service on test request

---
+++ src/js/services/request.js
@@ -0,0 +1,11 @@
+const getText = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+};
+
+export { getText };
